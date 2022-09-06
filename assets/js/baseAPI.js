789ca8adc9b7906068c913304092098e157b1ea6
LToA <965547672@qq.com>
2022-09-06 13:32:47 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,21 @@
 $.ajaxPrefilter(function (options) {
     options.url = 'http://www.liulongbin.top:3007' + options.url
     // console.log(options.url)
+    // 统一为有权限的接口，设置headers请求头
+    if (options.url.indexOf('/my')) {
+        options.headers = {
+            Authorization: localStorage.getItem('token')
+        }
+    }
+
+    options.complete = function (res) {
+        // console.log('执行了complete回调')
+        // console.log(res)
+        if (res.responseJSON.status === 1) {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+
+    }
+
 })
